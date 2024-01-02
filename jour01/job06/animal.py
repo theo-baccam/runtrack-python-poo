e3418bd2a868ff9ea262a15a5e500f5aabc76135
theo-baccam <theo.baccam@laplateforme.io>
2024-01-02 10:45:22 +0100

    01-06 commentaire et re-fait import

--- jour01/job06/animal.py
+++ jour01/job06/animal.py
@@ -1,10 +1,13 @@
 class Animal:
+    # Init contient l'âge, et le nom, qui pour le moment est rien
     def __init__(self):
         self.age = 0
         self.prenom = None
 
+    # Méthode pour augment l'âge
     def vieillir(self):
         self.age += 1
-
+    
+    # Méthode pour nommer l'animal
     def nommer(self, prenom):
         self.prenom = prenom
--- jour01/job06/main.py
+++ jour01/job06/main.py
@@ -1,13 +1,19 @@
-import animal
+from animal import Animal
 
-chat = animal.Animal()
+# On instancie un nouvel animal
+chat = Animal()
 
+# On affiche son âge
 print(chat.age)
 
+# On le fait vieillir
 chat.vieillir()
 
+# On re-affiche l'âge
 print(chat.age)
 
+# On nomme le chat
 chat.nommer("Zia")
 
+# On affiche le prénom
 print(chat.prenom)
