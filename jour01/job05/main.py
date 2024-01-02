1b27aff0a99e769526f3762718436c6732295760
theo-baccam <theo.baccam@laplateforme.io>
2024-01-02 10:42:46 +0100

    01-05 commentaire et re-fait import

--- jour01/job05/main.py
+++ jour01/job05/main.py
@@ -1,20 +1,22 @@
-import point
+from point import Point
 
-point_1 = point.Point(0, 0)
+# On créer un point et on lui donne une position
+point_1 = Point(0, 0)
 
+# On imprime les coordonnées
 point_1_coordinate = point_1.afficherLesPoints()
-
 print(point_1_coordinate)
 
+# On imprime les positions sur les deux axes séparément
 point_1_x = point_1.afficherX()
 point_1_y = point_1.afficherY()
-
 print(point_1_x, point_1_y)
 
+# On change la position du point
 point_1.changerX(1)
 point_1.changerY(1)
 
+# On imprime les nouvelles positions
 point_1_x = point_1.afficherX()
 point_1_y = point_1.afficherY()
-
 print(point_1_x, point_1_y)
--- jour01/job05/point.py
+++ jour01/job05/point.py
@@ -1,18 +1,22 @@
 class Point:
+    # Les attributs init contiennent les positions x et y
     def __init__(self, x_input, y_input):
         self.x = x_input
         self.y = y_input
 
+    # Méthode pour qui return la position sous forme de tuple
     def afficherLesPoints(self):
-        coordinate = [self.x, self.y]
+        coordinate = (self.x, self.y)
         return coordinate
 
+    # Méthodes qui donnent les positions dans l'axe X et Y séparément
     def afficherX(self):
         return self.x
 
     def afficherY(self):
         return self.y
 
+    # Méthodes qui permettent de changer x et y
     def changerX(self, increment):
         self.x += increment
 
