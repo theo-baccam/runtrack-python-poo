b2a1956f491f504dce709b56ca1ff9f3b45c6c10
theo-baccam <theo.baccam@laplateforme.io>
2024-01-02 10:52:09 +0100

    01-08 commentaire et re-fait import

--- jour01/job08/cercle.py
+++ jour01/job08/cercle.py
@@ -1,12 +1,16 @@
+# On importe la constante pi à partir du module math
 from math import pi
 
 class Cercle:
+    # init ne contient que le rayon, on calcule le reste des attributs du cercle à partir du rayon
     def __init__(self, initial_rayon):
         self.rayon = initial_rayon
-
+    
+    # Méthode pour changer le rayon
     def changerRayon(self, input_integer):
         self.rayon = input_integer
 
+    # Méthode pour calculer les autres attributs
     def circonference(self):
         circonference = 2 * pi * self.rayon
         return circonference
@@ -19,6 +23,7 @@ class Cercle:
         diametre = self.rayon * 2
         return diametre
 
+    # Méthode pour afficher les informations sur le cercle
     def afficherInfos(self):
         info_string = (
             f"Rayon = {self.rayon}\n"
--- jour01/job08/main.py
+++ jour01/job08/main.py
@@ -1,9 +1,13 @@
-import cercle
+from cercle import Cercle
 
-cercle_1 = cercle.Cercle(5)
+# On instancie un nouveau cercle
+cercle_1 = Cercle(5)
 
+# On affiche les informations sur ce cercle
 cercle_1.afficherInfos()
 
+# On change le rayon
 cercle_1.changerRayon(53)
 
+# On re-affiche les informations sur le cercle
 cercle_1.afficherInfos()
