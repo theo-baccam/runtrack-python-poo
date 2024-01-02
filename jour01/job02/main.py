e200e26b424be07c9dba483179ac5049f618a5da
theo-baccam <theo.baccam@laplateforme.io>
2024-01-02 09:08:00 +0100

    01-02

---
+++ jour01/job02/main.py
@@ -0,0 +1,5 @@
+import operation
+
+op = operation.Operation()
+
+print(op.nombre1, op.nombre2)
---
+++ jour01/job02/operation.py
@@ -0,0 +1,5 @@
+class Operation:
+    def __init__(self):
+        self.nombre1 = 83
+        self.nombre2 = 21
+
