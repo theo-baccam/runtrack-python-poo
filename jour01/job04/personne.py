28694b559df4d59e3801f185d569a817273eecaf
theo-baccam <theo.baccam@laplateforme.io>
2024-01-02 11:03:19 +0100

    01-04 handle erreur

--- jour01/job04/personne.py
+++ jour01/job04/personne.py
@@ -4,6 +4,10 @@ class Personne:
         self.personne_nom = nom
         self.personne_prenom = prenom
 
+        # Si le nom ou le prénom n'est pas un string, mettre une erreur
+        if not isinstance(nom, str) or not isinstance(prenom, str):
+            raise ValueError("Nom ou Prénom doit être un string")
+
     # Méthode pour return une string qui présente la personne
     def SePresenter(self):
         presentation = f"Je suis {self.personne_prenom} {self.personne_nom}."
