90c3ae1bb56a6af3f4aa1b19b11562ccfaa1e03a
theo-baccam <theo.baccam@laplateforme.io>
2024-01-02 10:31:52 +0100

    01-01 commentaire et re-fait import

--- jour01/job01/main.py
+++ jour01/job01/main.py
@@ -1,5 +1,8 @@
-import operation
+# On importe la classe à partir du fichier qui la contient
+from operation import Operation
 
-op = operation.Operation()
+# On créer une instance
+op = Operation()
 
+# Puis on l'affiche  sur la console
 print(op)
--- jour01/job01/operation.py
+++ jour01/job01/operation.py
@@ -1,5 +1,5 @@
 class Operation:
+    # __init__ permet de spécifier des attributs lorsque une instance est crée
     def __init__(self):
         self.nombre1 = 83
         self.nombre2 = 21
-
