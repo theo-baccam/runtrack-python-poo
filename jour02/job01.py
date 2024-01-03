bf8ca9d52901571f33768d16fa062f4d75281987
theo-baccam <theo.baccam@laplateforme.io>
2024-01-03 14:00:28 +0100

    02-01 commentaire

--- jour02/job01.py
+++ jour02/job01.py
@@ -1,8 +1,10 @@
 class Rectangle:
+    # Nous initialisons des attributs privés longuers et largeurs
     def __init__(self, longueur, largeur):
         self.__longueur = longueur
         self.__largeur = largeur
 
+    # Afin de pouvoir intéragir avec les attributs privés, il faut utiliser des getters et setters.
     def get_longueur(self):
         return self.__longueur
 
@@ -15,13 +17,18 @@ class Rectangle:
     def set_largeur(self, valeur):
         self.__largeur = valeur
 
+
+# On crée un rectangle 10x5
 rectangle = Rectangle(10, 5)
 
+# On vérifie ses dimensions
 print(rectangle.get_longueur())
 print(rectangle.get_largeur())
 
+# On modifie ses dimensions
 rectangle.set_longueur(7)
 rectangle.set_largeur(19)
 
+# On re-vérifie ses dimensions
 print(rectangle.get_longueur())
 print(rectangle.get_largeur())
