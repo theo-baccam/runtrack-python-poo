560d69d4af6c919356386d5cff4222fae61b7b59
theo-baccam <theo.baccam@laplateforme.io>
2024-01-04 08:39:54 +0100

    02-06 commentaires

--- jour02/job06.py
+++ jour02/job06.py
@@ -1,4 +1,5 @@
 class Commande:
+    # On initialise le numéro de la commande et le dict contenant plâts et prix)
     def __init__(self, order_number, order_list):
         self.__order_number = order_number
         self.__order_list = order_list
@@ -7,18 +8,22 @@ class Commande:
         self.__TAUX_TVA = 20 / 100
         self.__tva = self.__calculate_tva()
 
+    # Pour calculer la tva
     def __calculate_tva(self):
         tva = self.__prix_ht * self.__TAUX_TVA
         return tva
 
+    # Pour calculer prix hors taxe
     def __calculate_prix_ht(self):
         prix_ht = 0
         for value in self.__order_list.values():
             prix_ht += value
         return prix_ht
 
+    # Ajouter quelque chose à la commande
     def add_to_order(self, item, price):
         self.__order_list[item] = price
+        # On recalcule le prix et la TVA
         self.__prix_ht = self.__calculate_prix_ht()
         self.__tva = self.__calculate_tva()
 
@@ -28,6 +33,7 @@ class Commande:
     def cancel_order(self):
         self.__order_status = "Annulé"
 
+    # Méthode pour obtenir la commande dans un format lisible
     def order_info(self):
         order_info = (
             f"Numéro: {self.__order_number}\n"
@@ -47,27 +53,33 @@ class Commande:
 
 
 def main():
+    # Première commande
     first_order = {
         "Large Peperonni Pizza": 9.20,
     }
     first =  Commande(1, first_order)
     print(first.order_info())
 
+    # Ajout
     first.add_to_order("3L Coke Bottle", 4.30)
     print(first.order_info())
 
+    # Annulé
     first.cancel_order()
     print(first.order_info())
 
+    # Deuxième commande
     second_order = {
         "Medium Pineapple Pizza": 8.50,
     }
     second = Commande(2, second_order)
     print(second.order_info())
 
+    # Ajout
     second.add_to_order("Canette de Sprite", 1.40)
     print(second.order_info())
 
+    # Terminé
     second.finish_order()
     print(second.order_info())
 
