0aeba1652466d29c65fd9da44b9d95831c7527ba
theo-baccam <theo.baccam@laplateforme.io>
2024-01-05 10:57:13 +0100

    04-04

---
+++ jour04/job04.py
@@ -0,0 +1,24 @@
+class Forme:
+    def aire(self):
+        return 0
+
+
+class Rectangle(Forme):
+    def __init__(self, longueur, largeur):
+        if not (
+            isinstance(longueur, (float, int))
+            and isinstance(largeur, (float, int))
+            and longueur > 0
+            and largeur > 0
+        ):
+            raise ValueError("Les dimensions doivent êtres des nombres positifs")
+        super().__init__()
+        self.__longueur = longueur
+        self.__largeur = largeur
+
+    def aire(self):
+        return self.__longueur * self.__largeur
+
+
+rectangle = Rectangle(6, 8)
+print(rectangle.aire())
